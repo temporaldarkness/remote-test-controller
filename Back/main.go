1007e31e692b6ec5acafe2677116a2fd30e1fe5a
Alexey <alexey.brysh@gmail.com>
2025-07-06 11:02:16 +0300

    front + basic back

--- Back/main.go
+++ Back/main.go
@@ -1,9 +1,76 @@
 package main
 
 import (
-    "fmt"
+    "encoding/json"
+    "log"
+    "net/http"
+    "sync"
+    "time"
+
+    "github.com/gorilla/websocket"
 )
 
+type State struct {
+    Running   bool      `json:"running"`
+    StartTime time.Time `json:"startTime,omitempty"`
+}
+
+var (
+    upgrader = websocket.Upgrader{
+        CheckOrigin: func(r *http.Request) bool { return true },
+    }
+    state      = State{}
+    stateMutex sync.Mutex
+)
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("Upgrade error:", err)
+        return
+    }
+    defer conn.Close()
+
+    for {
+        _, msg, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Read error:", err)
+            break
+        }
+
+        var req map[string]interface{}
+        if err := json.Unmarshal(msg, &req); err != nil {
+            continue
+        }
+
+        stateMutex.Lock()
+        switch req["action"] {
+        case "start":
+            state.Running = true
+            state.StartTime = time.Now()
+        case "stop":
+            state.Running = false
+            state.StartTime = time.Time{}
+        case "get_state":
+            // ничего не меняем
+        }
+        resp := state
+        stateMutex.Unlock()
+
+        // Формируем ответ
+        out := map[string]interface{}{
+            "running": resp.Running,
+        }
+        if resp.Running {
+            out["startTime"] = resp.StartTime.Format(time.RFC3339)
+        }
+        b, _ := json.Marshal(out)
+        conn.WriteMessage(websocket.TextMessage, b)
+    }
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/ws", wsHandler)
+    log.Println("WebSocket server started at :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
