467caeba21de0e07333cf0ae6915d056bd20d5ec
temporaldarkness <94239354+temporaldarkness@users.noreply.github.com>
2025-07-08 07:46:23 +0300

    Эндпоинты для аппаратных тестов
    Добавляет чёткие функции, к которым можно присоединиться в прикладном применении программы.
    Помимо этого немного обновляет вывод и добавляет условие для паузы.

--- Back/main.go
+++ Back/main.go
@@ -24,6 +24,7 @@ var (
 	
 	infoLog    = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	errorLog   = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	hardwareLog   = log.New(os.Stdout, "HARDWARE: ", log.Ldate|log.Ltime|log.Lshortfile)
 )
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -59,25 +60,32 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		switch action {
 			case "start":
 				if !state.Running {
-					infoLog.Printf("[%s] Performing action: Start", conn.RemoteAddr())
+					infoLog.Printf("[%s] Performing action: Start (Startup)", conn.RemoteAddr())
 					state.Running = true
 					state.StartTime = time.Now().UTC()
 					state.PausedAt = time.Time{}
+					hardwareStart()
 				} else if !state.PausedAt.IsZero() {
+					infoLog.Printf("[%s] Performing action: Start (Unpause)", conn.RemoteAddr())
 					now := time.Now().UTC()
 					state.StartTime = state.StartTime.Add(now.Sub(state.PausedAt))
 					state.PausedAt = time.Time{}
+					hardwareUnpause()
 				}
 			case "pause":
-				infoLog.Printf("[%s] Performing action: Pause", conn.RemoteAddr())
 				if state.Running && state.PausedAt.IsZero() {
+					infoLog.Printf("[%s] Performing action: Pause", conn.RemoteAddr())
 					state.PausedAt = time.Now().UTC()
+					hardwarePause()
 				}
 			case "stop":
-				infoLog.Printf("[%s] Performing action: Stop", conn.RemoteAddr())
-				state.Running = false
-				state.StartTime = time.Time{}
-				state.PausedAt = time.Time{}
+				if state.Running {
+					infoLog.Printf("[%s] Performing action: Stop", conn.RemoteAddr())
+					state.Running = false
+					state.StartTime = time.Time{}
+					state.PausedAt = time.Time{}
+					hardwareStop()
+				}
 			default:
 				infoLog.Printf("[%s] Unknown action: %v", conn.RemoteAddr(), action)
 		}
@@ -104,6 +112,22 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			errorLog.Printf("[%s] Response writing error: %v", conn.RemoteAddr(), err)
 		}
 	}
+}
+
+func hardwareStart() {
+	hardwareLog.Printf("Hardware test start!")
+}
+
+func hardwareStop() {
+	hardwareLog.Printf("Hardware test stop!")
+}
+
+func hardwarePause() {
+	hardwareLog.Printf("Hardware test pause!")
+}
+
+func hardwareUnpause() {
+	hardwareLog.Printf("Hardware test unpause!")
 }
 
 func main() {
